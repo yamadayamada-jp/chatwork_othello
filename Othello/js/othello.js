a07b7063273174fe09eb04635388962d8af5aa27
Yamada Koki <yamada.koki@hamee.co.jp>
2016-02-27 14:42:57 +0900

    othelloボタンを押すとコンソールにmessageが流れるようにした

--- Othello/js/othello.js
+++ Othello/js/othello.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+var Othello = function(){
+
+    this.button_id = 'othello';
+
+    this.start = function(){
+        console.log('start');
+    };
+};
+
+
+$(document).ready(function(){
+    var othello = new Othello();
+
+    var othello_button_li = '<li id='+othello.button_id+' role="button" class="_showDescription icoFont" aria-label="チャットワークオセロ" style="font-size:13px;">[オセロ]</li>';
+    $('#_chatSendTool').append(othello_button_li);
+
+    $("#"+othello.button_id).on('click', function() {
+        othello.start();
+    });
+});
